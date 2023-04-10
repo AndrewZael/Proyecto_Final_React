2dc19d13dde069e69ebd327b0d19ce190c63fab5
H514226 <aguzmanv@forum.cl>
2023-04-10 16:25:17 -0400

    Ajuste links footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import Instagram from '../components/icons-svg/Instagram';
 import Facebook from '../components/icons-svg/Facebook';
 import Linkedin from '../components/icons-svg/Linkedin';
 import Context from '../contexts/Context';
+import { Link, NavLink } from 'react-router-dom';
 
 const Footer = () => {
   const { userLogin } = useContext(Context);   
@@ -18,18 +19,22 @@ const Footer = () => {
                 </div>
                 <nav className='col-12 col-sm-6 col-md-3 mb-3 mb-md-0'>
                     <ul className='text-light'>
-                        <li><a href='https://www.google.com/' className='text-light text-decoration-none small'>HOME</a></li>
-                        <li><a href='https://www.google.com/' className='text-light text-decoration-none small'>PUBLICACIONES</a></li>
+                        <li><NavLink to='/' className='text-light text-decoration-none small'>
+                            HOME</NavLink></li>
+                        <li><NavLink to='/publicaciones' className='text-light text-decoration-none small'>PUBLICACIONES</NavLink></li>
                     </ul>
                 </nav>
                 {
                     userLogin ? 
                     <nav className='col-12 col-sm-6 col-md-3 mb-3 mb-md-0'>
                         <ul className='text-light'>
-                            <li><a href='https://www.google.com/' className='text-light text-decoration-none small'>MI PERFIL</a></li>
-                            <li><a href='https://www.google.com/' className='text-light text-decoration-none small'>MIS FAVORITOS</a></li>
-                            <li><a href='https://www.google.com/' className='text-light text-decoration-none small'>MIS PUBLICACIONES</a></li>
-                            <li><a href='https://www.google.com/' className='text-light text-decoration-none small'>NUEVA PUBLICACIÓN</a></li>
+                            <li><NavLink to='/perfil/mis-datos' className='text-light text-decoration-none small'>MI PERFIL</NavLink></li>
+                            <li><NavLink to='/perfil/favoritos' className='text-light text-decoration-none small'>
+                                MIS FAVORITOS</NavLink></li>
+                            <li><NavLink to='/perfil/mis-publicaciones' className='text-light text-decoration-none small'>
+                                MIS PUBLICACIONES</NavLink></li>
+                            <li><NavLink to='/perfil/nueva-publicacion' className='text-light text-decoration-none small'>
+                                NUEVA PUBLICACIÓN</NavLink></li>
                         </ul>
                     </nav> : null
                 }
@@ -41,17 +46,22 @@ const Footer = () => {
             </div>
             <div className='col-12 col-lg-4 col-xl-6 d-flex flex-column flex-sm-row flex-xl-column align-items-center align-items-xl-end justify-content-between mb-4'>
                 <nav className='d-flex flex-column flex-sm-row flex-lg-column flex-xl-row gap-2 mb-4 mb-sm-0'>
-                    <a href='https://www.google.com/' className='btn btn-sm px-3 rounded-pill btn-outline-light'>REGISTRATE</a>
-                    <a href='https://www.google.com/' className='btn btn-sm px-3 rounded-pill btn-outline-light'>INGRESA</a>
+                    {
+                        !userLogin ? 
+                        <>
+                            <Link to='/registro' className='btn btn-sm px-3 rounded-pill btn-outline-light'>REGISTRATE</Link>
+                            <Link to='/login' className='btn btn-sm px-3 rounded-pill btn-outline-light'>INGRESA</Link>
+                        </> : null
+                    }
                     {
                         userLogin ? 
                         <a href='https://www.google.com/' className='btn btn-sm px-3 rounded-pill btn-auxiliar text-light'>CONVIERTETE EN PRO</a> : null
                     }
                 </nav>
                 <nav className='d-flex gap-2 mb-4 mb-sm-0'>
-                    <a href='https://www.google.com/'><Instagram color="#fcfcfc" size={32} /></a>
-                    <a href='https://www.google.com/'><Facebook color="#fcfcfc" size={32} /></a>
-                    <a href='https://www.google.com/'><Linkedin color="#fcfcfc" size={32} /></a>
+                    <a href='https://www.instagram.com/' target='_blank' rel='noopener noreferrer'><Instagram color="#fcfcfc" size={32} /></a>
+                    <a href='https://www.facebook.com/' target='_blank' rel='noopener noreferrer'><Facebook color="#fcfcfc" size={32} /></a>
+                    <a href='https://www.linkedin.com/' target='_blank' rel='noopener noreferrer'><Linkedin color="#fcfcfc" size={32} /></a>
                 </nav>
             </div>
        </div>
